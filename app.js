a7eac48b78b586f8cf3e75810de76195354303f0
Lane Harrison <lanetharrison@gmail.com>
2013-06-13 13:16:07 -0400

    more simplification

--- app.js
+++ app.js
@@ -12,28 +12,29 @@ var express     = require('express')
       cert: fs.readFileSync('ssl/server.crt')
     }
 
+// Redis init and behavior
 var redisClient = redis.createClient()
   .on('connect', function() {
     console.log('Connected to redis.')
   })
+function save(d) {
+  redisClient.hmset(d.postId, d)
+  console.log('saved to redis: ' + d.postId)
+}
 
+// Express init and behavior
 var app = express()
   .use(express.static(__dirname + '/public'))
   .use(express.bodyParser())
   .use(allowCrossDomain)
   .post('/', handlePost)
   .options('/', sendOK)
-
 function handlePost(req, res) {
   var d = req.body
   d.postId = (+new Date()).toString(36)
   d.timestamp = (new Date()).getTime()
   save(d)
   res.send(200)
-}
-function save(d) {
-  redisClient.hmset(d.postId, d)
-  console.log('saved to redis: ' + d.postId)
 }
 function sendOK(req, res) {
   res.send(200);
@@ -44,21 +45,25 @@ function allowCrossDomain(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Length, Content-Type, Date')
   next()
 }
-
+// host config and server creation
 if( host === 'appfog' ) {
-  http.createServer(app).listen(process.env.VCAP_APP_PORT || 3000, function (err) {
-    if (!err) console.log('Listening on port ' + process.env.VCAP_APP_PORT || 3000)
-  })
+  createServer(process.env.VCAP_APP_PORT || 3000)
 }
 if( host === 'local' ){
-  http.createServer(app).listen(80, function (err) {
-    if (!err) console.log('Listening on port 80')
-  })
-  https.createServer(sslOptions, app).listen(443, function (err) {
-    if (!err) console.log('Listening on port 443')
-  })
+  createServer(80)
+  createServer(443, sslOptions)
+}
+function createServer(port, opts) {
+  if(opts)
+    https.createServer(opts, app).listen(port, handleErr)
+  else 
+    http.createServer(app).listen(port, handleErr)
+  function handleErr(err) {
+    if (!err) console.log('Listening on port ' + port)
+  }
 }
 
+// Handle when a user doesn't use sudo
 process.on('uncaughtException', function (err) {
   if (err.code === 'EACCES') 
     console.log('Unable to start server - you must start as root user.')
