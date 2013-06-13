100c4a31aaeda6b6e6532d5dcafa951bd5eeb910
Lane Harrison <lanetharrison@gmail.com>
2013-06-13 13:02:13 -0400

    simplified further; removed POST error handling, since it will appear in the browser

--- app.js
+++ app.js
@@ -4,50 +4,46 @@
 var express     = require('express')
   , https       = require('https')
   , http        = require('http')
-  , fs          = require('fs')
   , redis       = require('redis')
-  , redisClient
-  , dataDir     = 'userdata/'
   , host        = 'local' // 'appfog' or 'local'
+  , fs          = require('fs')
   , sslOptions  = {
       key: fs.readFileSync('ssl/server.key'),
       cert: fs.readFileSync('ssl/server.crt')
     }
 
+var redisClient = redis.createClient()
+  .on('connect', function() {
+    console.log('Connected to redis.')
+  })
+
 var app = express()
   .use(express.static(__dirname + '/public'))
   .use(express.bodyParser())
-  .options('/', function(req, res) { res.send(200) })
-  .post('/', function handlePost(req, res) {
-    var d = req.body
-    d.postId = (+new Date()).toString(36)
-    d.timestamp = (new Date()).getTime()
-    saveRedis(d)
-    res.send(200)
-  })
-  .post('/error', function handlePost(req, res) {
-    var d = req.body
-    console.log(d.msg)
-    res.send(200)
-  })
+  .use(allowCrossDomain)
+  .post('/', handlePost)
+  .options('/', sendOK)
 
-var allowCrossDomain = function allowCrossDomain(req, res, next) {
+function handlePost(req, res) {
+  var d = req.body
+  d.postId = (+new Date()).toString(36)
+  d.timestamp = (new Date()).getTime()
+  save(d)
+  res.send(200)
+}
+function save(d) {
+  redisClient.hmset(d.postId, d)
+  console.log('saved to redis: ' + d.postId)
+}
+function sendOK(req, res) {
+  res.send(200);
+}
+function allowCrossDomain(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   res.header('Access-Control-Allow-Headers', 'Content-Length, Content-Type, Date')
   next()
 }
-app.use(allowCrossDomain)
-
-var saveRedis = function saveRedis(d) {
-  redisClient.hmset(d.postId, d)
-  console.log('saved to redis: ' + d.postId)
-}
-
-redisClient = redis.createClient()
-redisClient.on('connect', function() {
-  console.log('Connected to redis.')
-})
 
 if( host === 'appfog' ) {
   http.createServer(app).listen(process.env.VCAP_APP_PORT || 3000, function (err) {
