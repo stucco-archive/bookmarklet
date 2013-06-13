7f6af02033eb2fdf81d4ab03f36cd25a93694d9a
Lane Harrison <lanetharrison@gmail.com>
2013-06-13 12:26:57 -0400

    killed semicolons. I want to node right.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* global require:true, console:true, process:true, __dirname:true */
-'use strict';
+'use strict'
 
 var express  = require('express')
   , https    = require('https')
@@ -9,63 +9,62 @@ var express  = require('express')
   , fs       = require('fs')
   , redis    = require('redis')
   , redisClient
-  , dataDir  = 'userdata/';
+  , dataDir  = 'userdata/'
 
-
-var output = 'redis'; // 'csv' or 'redis'
-var host = 'local'; // 'appfog' or 'local'
+var output = 'redis' // 'csv' or 'redis'
+var host = 'local' // 'appfog' or 'local'
 
 // setup for redis
 if (output === 'redis') {
-  redisClient = redis.createClient();
+  redisClient = redis.createClient()
 
   // redis connection test
   redisClient.on('connect', function() {
-    console.log('Connected to redis.');
-  });
+    console.log('Connected to redis.')
+  })
 }
 
 // CORS middleware
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Length, Content-Type, Date');
-  next();
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Length, Content-Type, Date')
+  next()
 }
 
 // Init
-var app = express();
-app.use(express.bodyParser());
-app.use(allowCrossDomain);
-app.use(express.static(__dirname + '/public'));
+var app = express()
+app.use(express.bodyParser())
+app.use(allowCrossDomain)
+app.use(express.static(__dirname + '/public'))
 
 // OPTIONS (NOTE: necessary for https response)
 app.options('/', function(req, res) {
-  res.send(200);
-});
+  res.send(200)
+})
 
 // POST
 app.post('/', function handlePost(req, res) {
-  var d = req.body;
-  d.postId = (+new Date()).toString(36);
-  d.timestamp = (new Date()).getTime();
+  var d = req.body
+  d.postId = (+new Date()).toString(36)
+  d.timestamp = (new Date()).getTime()
   if(output === 'csv')
-    saveCSV(d.postId+'.csv', d);
+    saveCSV(d.postId+'.csv', d)
   if(output === 'redis')
-    saveRedis(d);
-  res.send(200);
+    saveRedis(d)
+  res.send(200)
 })
 
 // POST (error)
 app.post('/error', function handlePost(req, res) {
-  var d = req.body;
-  console.log(d.msg);
-  res.send(200);
+  var d = req.body
+  console.log(d.msg)
+  res.send(200)
 })
 
 var saveRedis = function saveRedis(d) {
-  redisClient.hmset(d.postId, d);
-  console.log('saved to redis: ' + d.postId);
+  redisClient.hmset(d.postId, d)
+  console.log('saved to redis: ' + d.postId)
 }
 
 // Process form
@@ -74,11 +73,11 @@ var saveCSV = function saveCSV(name, json) {
   var params = { 
     data: [json], 
     fields: Object.keys(json) 
-  };
+  }
   json2csv(params, function(err, csvData) {
-    if (err) throw err;
-    csv().from(csvData).to(dataDir+name);
-    console.log('csv saved, %s', name);
+    if (err) throw err
+    csv().from(csvData).to(dataDir+name)
+    console.log('csv saved, %s', name)
   })
 }
 
@@ -86,35 +85,35 @@ var saveCSV = function saveCSV(name, json) {
 var sslOptions = {
   key: fs.readFileSync('ssl/server.key'),
   cert: fs.readFileSync('ssl/server.crt')
-};
+}
 
 if( host === 'appfog' ) {
   http.createServer(app).listen(process.env.VCAP_APP_PORT || 3000, function (err) {
     if (!err) {
-      console.log('Listening on port ' + process.env.VCAP_APP_PORT || 3000);
+      console.log('Listening on port ' + process.env.VCAP_APP_PORT || 3000)
     }
-  });
+  })
 }
 
 if( host === 'local' ){
   http.createServer(app).listen(80, function (err) {
     if (!err) {
-      console.log('Listening on port 80');
+      console.log('Listening on port 80')
     }
-  });
+  })
   https.createServer(sslOptions, app).listen(443, function (err) {
     if (!err) {
-      console.log('Listening on port 443');
+      console.log('Listening on port 443')
     }
-  });
+  })
 }
 
 process.on('uncaughtException', function (err) {
   if (err.code === 'EACCES') {
-    console.log('Unable to start server - you must start as root user.');
+    console.log('Unable to start server - you must start as root user.')
   }
   else {
-    console.log(err);
+    console.log(err)
   }
-  process.exit(1);
-});
+  process.exit(1)
+})
